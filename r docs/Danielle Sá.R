056cb925501cc40c1a89a1e33e02508bf8073649
Danielle <222007101@aluno.unb.br>
2024-04-23 23:55:05 -0300

    ..

---
+++ r docs/Danielle Sá.R
@@ -0,0 +1,5 @@
+###################Projeto Fantasma##############
+library(tidyverse)
+banco <- read.csv("banco_final.csv")
+# Análise 1:Número de lançamentos a cada década por formato de lançamento
+
