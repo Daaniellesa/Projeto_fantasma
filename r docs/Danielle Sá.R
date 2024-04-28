a04c581b97728111e4dbcaab5d3aff7c5d81cb0b
Daaniellesa <222007101@aluno.unb.br>
2024-04-28 02:21:49 -0300

    .

--- r docs/Danielle Sá.R
+++ r docs/Danielle Sá.R
@@ -35,3 +35,41 @@ theme_estat <- function(...) {
 
 # Análise 1:Número de lançamentos a cada década por formato de lançamento
 
+banco <- banco %>%
+  mutate(date_aired = as.Date(date_aired, format = "%Y-%m-%d"))
+
+banco <- banco %>%
+  mutate(AnoLancamento = year(date_aired),  
+         Década = floor(AnoLancamento / 10) * 10)
+
+lançamentos <- banco %>%
+  group_by(Década, format) %>%
+  summarise(freq = n()) %>%
+  mutate(freq_relativa = round(freq / sum(freq) * 100, 1))
+
+porcentagens <- paste(lançamentos$freq_relativa, "%")
+legendas <- paste(lançamentos$freq, " (", porcentagens, ")")
+
+lançamentos$Década <- as.character(lançamentos$Década)
+
+
+ggplot(lançamentos) +
+  aes(
+    x = fct_reorder(Década, freq, .desc = TRUE), 
+    y = freq,
+    fill = format, 
+    label = porcentagens
+  ) +
+  geom_col(position = "dodge") +
+  geom_text(
+    position = position_dodge(width = 0.9),
+    vjust = -0.3, hjust = 0.3,
+    size = 3
+  ) +
+  labs(
+    x = "Décadas",
+    y = "Frequência de Lançamentos",
+    fill = "Formato de Lançamento"
+  ) +
+  theme_estat()
+ggsave("colunas-bi-freq.pdf", width = 158, height = 93, units = "mm")
